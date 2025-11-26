1c75d2875b6aa79b3d13324ff5547dd656429d33
ricardo-deazeredo <ricardo.azeredoaz@gmail.com>
2025-11-26 13:53:51 +0000

    aula quase concluida

---
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -0,0 +1,42 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
+
+@Configuration
+@ComponentScan(basePackages = "com.example.demo")
+public class WebConfig implements WebMvcConfigurer{
+
+    @Bean
+    public SpringTemplateEngine templateEngine() {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.addDialect(new LayoutDialect());
+        templateEngine.setTemplateResolver(thymeleafTemplateResolver());
+        return templateEngine;
+    }
+
+    @Bean
+    public SpringResourceTemplateResolver thymeleafTemplateResolver() {
+        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setTemplateMode("HTML");
+        templateResolver.setPrefix("classpath:/templates");
+        templateResolver.setSuffix(".html");
+        templateResolver.setCharacterEncoding("UTF-8");
+        templateResolver.setCacheable(false);
+        return templateResolver;
+    }
+
+    @Bean
+    public thymeleafTemplateResolver thymeleafTemplateResolver(){
+        ThymeleafTemplateResolver viewResolver = new ThymeleafTemplateResolver();
+        viewResolver.setTemplateEngine();
+        return viewResolver;
+
+    }
+
+    
+}
